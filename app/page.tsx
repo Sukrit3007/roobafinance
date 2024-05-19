26b866a22b548fd1271906622b623301162af441
Sukrit <sukrit3007@outlook.com>
2024-05-20 02:18:12 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
 			{loading ? (
 				<section className="min-h-screen grid grid-cols-1 lg:grid-cols-3 overflow-hidden">
 					<div className="relative min-h-screen md:col-span-2">
-						<div className="hidden md:inline min-h-screen absolute z-50 right-0 top-0">
+						<div className="hidden lg:inline min-h-screen absolute z-50 right-0 top-0">
 							<ScrolllingText />
 						</div>
 						<Hero />
